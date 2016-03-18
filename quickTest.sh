ef96ddc6c03ba5d0e9f48abe1f97197a4111769a
Cybera Sysadmin <sysadmin@cybera.ca>
2016-03-18 00:06:42 +0000

    Don't run heat and swift unless it's actually enabled

--- quickTest.sh
+++ quickTest.sh
@@ -3,7 +3,7 @@
 # CRUD of most basic user facing keystone, nova, cinder, glance items
 rally task start basic-kncg.json --task-args-file args.json
 
-# Swift, Heat
-rally task start swift.json
-rally taks start heat.json
+# Swift, Heat - Enable if required
+#rally task start swift.json
+#rally taks start heat.json
 
